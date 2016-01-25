d1a9e83e1931897861814b349fd964185674b204
Anna Becker and Jordan Meier <jbirdmeier@gmail.com>
2016-01-25 09:57:44 -0800

    Add functionality to user logic interface in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
-function Place(location, landmarks, timeYear, notes) {
+function Place(location, landmark, timeYear, notes) {
   this.location = location;
-  this.landmarks = landmarks;
+  this.landmark = landmark;
   this.timeYear = timeYear;
   this.notes = "";
 }
+
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+
+    var inputLocation = $("input#new-location").val();
+    var inputLandmark = $("input#new-landmark").val();
+    var inputTimeYear = $("input#new-timeYear").val();
+    var inputNotes = $("input#new-notes").val();
+    var newPlace = new Place(inputLocation, inputLandmark, inputTimeYear, inputNotes);
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
+
+    $("input#new-location").val("");
+    $("input#new-landmark").val("");
+    $("input#new-timeYear").val("");
+    $("input#new-notes").val("");
+
+    $(".place").last().click(function() {
+      $("#show-places").show();
+      $("#show-places h2").text(newPlace.location);
+      $(".location").text(newPlace.location);
+      $(".landmark").text(newPlace.landmark);
+      $(".timeYear").text(newPlace.timeYear);
+      $(".notes").text(newPlace.notes);
+    });
+
+    event.preventDefault();
+  });
+
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 describe('Place', function () {
   it("creates a new Place with the given properties", function () {
-    var testPlace = new Place("Florence", ["Duomo","David","Ponte Vecchio"], "Spring", "");
+    var testPlace = new Place("Florence", "Duomo", "Spring", "");
     expect(testPlace.location).to.equal("Florence");
-    expect(testPlace.landmarks).to.eql(["Duomo","David","Ponte Vecchio"]);
+    expect(testPlace.landmark).to.eql("Duomo");
     expect(testPlace.timeYear).to.equal("Spring");
     expect(testPlace.notes).to.equal("");
   });
